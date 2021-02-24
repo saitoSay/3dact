c388fd392ed96ae40dd4a6924f9a677393810c54
saitoSay <seinyacchi@gmail.com>
2021-02-24 18:20:26 +0900

    タイトルのフェードアウトを実装した

---
+++ Assets/Scripts/FadeoutController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeoutController : MonoBehaviour
+{
+    Animator anim;
+    bool frag = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        frag = false;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown && !frag)
+        {
+            anim.SetBool("Trigger", true);
+            frag = true;
+        }
+    }
+}
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -2,8 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
 public class SceneController : MonoBehaviour
 {
+    AudioSource audioSource;
+    [SerializeField] AudioClip sound;
+    Animator anim;
+    bool frag = false;
+    
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        frag = false;
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void Update()
+    {
+        if (Input.anyKeyDown && !frag)
+        {
+            anim.SetBool("Trigger", true);
+            frag = true;
+            audioSource.PlayOneShot(sound);
+        }
+    }
     public void LordGameScene()
     {
         SceneManager.LoadScene(1);
