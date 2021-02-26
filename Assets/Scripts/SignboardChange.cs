bd439f43c149fc0a72a648ae43c5fb462b95e37c
saitoSay <seinyacchi@gmail.com>
2021-02-26 22:39:37 +0900

    操作説明用の看板を設置した

---
+++ Assets/Scripts/SignboardChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignboardChange : MonoBehaviour
+{
+    Animator m_anim;
+    void Start()
+    {
+        m_anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (SignboardController.s_boradFrag == 1)
+        {
+            m_anim.Play("boradShow");
+        }
+        else if(SignboardController.s_boradFrag == 2)
+        {
+            m_anim.Play("boradClose");
+        }
+    }
+
+}
